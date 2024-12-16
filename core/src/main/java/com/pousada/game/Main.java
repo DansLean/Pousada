c7ecf126e5c7e646a6f9fe17f8a597f1753e2582
itsjguilherme <joaoggfhu@gmail.com>
2024-12-16 17:55:21 -0300

    feat: adicionando o background do jogo

--- core/src/main/java/com/pousada/game/Main.java
+++ core/src/main/java/com/pousada/game/Main.java
@@ -1,34 +1,95 @@
 package com.pousada.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
-import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
+import com.badlogic.gdx.audio.Sound;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.MathUtils;
+import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.ScreenUtils;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.ui.ImageButton;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 
 /** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
 public class Main extends ApplicationAdapter {
-    private SpriteBatch batch;
-    private Texture image;
+    Texture backgroundTexture;
+    SpriteBatch spriteBatch;
+    FitViewport viewport;
+    Texture normalButon;
+    Texture pressedButon;
+    ImageButton button;
+    TextureRegionDrawable normalDrawable;
+    TextureRegionDrawable pressedDrawable;
 
     @Override
     public void create() {
-        batch = new SpriteBatch();
-        image = new Texture("libgdx.png");
+        spriteBatch = new SpriteBatch();
+        backgroundTexture = new Texture("Background.png");
+        viewport = new FitViewport(8, 5);
+        normalButon = new Texture("buttons/botao_normal.png");
+        pressedButon = new Texture("buttons/botao_press.png");
+        normalDrawable = new TextureRegionDrawable(new TextureRegion(normalButon));
+        pressedDrawable = new TextureRegionDrawable(new TextureRegion(pressedButon));
+
+        ImageButton.ImageButtonStyle buttonStyle = new ImageButton.ImageButtonStyle();
+        buttonStyle.up = normalDrawable;
+        buttonStyle.down = pressedDrawable;
+
+
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(com.badlogic.gdx.input.InputEvent event, float x, float y) {
+                System.out.println("Botão de imagem clicado!");
+            }
+        });
+
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        viewport.update(width, height, true);
     }
 
     @Override
     public void render() {
-        ScreenUtils.clear(0.15f, 0.15f, 0.2f, 1f);
-        batch.begin();
-        batch.draw(image, 140, 210);
-        batch.end();
+        draw();
     }
 
     @Override
     public void dispose() {
-        batch.dispose();
-        image.dispose();
+        spriteBatch.dispose();
+        backgroundTexture.dispose();
+    }
+
+    private void draw() {
+        ScreenUtils.clear(0.1686f, 0.1686f, 0.1686f, 1f);
+        viewport.apply();
+        spriteBatch.setProjectionMatrix(viewport.getCamera().combined);
+        spriteBatch.begin();
+
+        float worldWidth = viewport.getWorldWidth();
+        float worldHeight = viewport.getWorldHeight();
+
+        spriteBatch.draw(backgroundTexture, 0, 0, worldWidth, worldHeight);
+        button = new ImageButton(buttonStyle);
+        button.setPosition(200, 200);
+
+        spriteBatch.end();
     }
 }
