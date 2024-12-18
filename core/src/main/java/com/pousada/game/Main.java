34f773dfec4e1bcd56e50fb6d7486e54f9082f85
itsjguilherme <joaoggfhu@gmail.com>
2024-12-18 00:26:45 -0300

    fix: resolvendo bugs

--- core/src/main/java/com/pousada/game/Main.java
+++ core/src/main/java/com/pousada/game/Main.java
@@ -3,84 +3,107 @@ package com.pousada.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
+import com.badlogic.gdx.scenes.scene2d.*;
+import com.badlogic.gdx.scenes.scene2d.ui.*;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.scenes.scene2d.utils.TextureRegionDrawable;
 import com.badlogic.gdx.utils.ScreenUtils;
-import com.badlogic.gdx.scenes.scene2d.Stage;
-import com.badlogic.gdx.scenes.scene2d.ui.Label;
-import com.badlogic.gdx.scenes.scene2d.ui.ScrollPane;
-import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
 
 public class Main extends ApplicationAdapter {
     SpriteBatch spriteBatch;
     Stage stage;
+    Texture textureBackGround;
+    Texture buttonNormalTexture, buttonPressedTexture;
+    ImageButton button;
     BitmapFont textLabel;
     Label logLabel;
     StringBuilder logs;
     Character personagem;
-
-    // Criando a área rolável para os logs
     ScrollPane scrollPane;
+    Table rootTable;
 
     @Override
     public void create() {
         spriteBatch = new SpriteBatch();
-        stage = new Stage();
+        stage = new Stage(new StretchViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight()));
         Gdx.input.setInputProcessor(stage);
+
+        textureBackGround = new Texture("Background.png");
+        Image backgroundImage = new Image(textureBackGround);
+        backgroundImage.setPosition(0, Gdx.graphics.getHeight() - 440);
+        backgroundImage.setSize(700, 450);
+        backgroundImage.setTouchable(Touchable.disabled);
+        stage.addActor(backgroundImage);
+
+        personagem = new Character(205, 42);
+        rootTable = new Table();
+        rootTable.setFillParent(true);
 
         textLabel = criarFonte(14, Color.WHITE);
         logs = new StringBuilder();
 
-        // Criação do Label para exibir os logs
         logLabel = new Label("", new Label.LabelStyle(textLabel, Color.WHITE));
+        exemploDeLogs();
 
-        // Criando uma Table para o log, que servirá como container para o ScrollPane
         Table logTable = new Table();
-        logTable.top().right().pad(10); // Ajuste a posição
+        logTable.top().right().pad(10);
         logTable.add(logLabel).expand().top().right().pad(10);
 
-        // Criando o ScrollPane para permitir rolagem do campo de logs
         scrollPane = new ScrollPane(logTable);
-//        scrollPane.setFillParent(true); // Faz o ScrollPane ocupar todo o tamanho disponível
-        scrollPane.setForceScroll(false, true); // Permitir rolagem vertical, mas não horizontal
-//        stage.addActor(scrollPane); // Adiciona o ScrollPane ao stage
+        scrollPane.setForceScroll(false, true);
 
-        Table rootTable = new Table();
-        rootTable.setFillParent(true);
         rootTable.add(scrollPane).expand().fill();
 
         stage.addActor(rootTable);
 
-        drawBackground();
-        drawButton();
-        exemploDeLogs();
+        buttonNormalTexture = new Texture("button_normal.png");
+        buttonPressedTexture = new Texture("button_pressed.png");
 
-        personagem = new Character(205, 42);
+        TextureRegionDrawable buttonNormal = new TextureRegionDrawable(new TextureRegion(buttonNormalTexture));
+        TextureRegionDrawable buttonPressed = new TextureRegionDrawable(new TextureRegion(buttonPressedTexture));
+
+        button = new ImageButton(buttonNormal, buttonPressed);
+        button.setSize(65, 40);
+
+        float buttonX = 325;
+        float buttonY = Gdx.graphics.getHeight() - 470;
+        button.setPosition(buttonX, buttonY);
+
+        button.addListener(new ClickListener() {
+            @Override
+            public void clicked(InputEvent event, float x, float y) {
+                System.out.println("Botão clicado!");
+                adicionarLog("Botão clicado!");
+            }
+        });
+
+        stage.addActor(button);
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        stage.getViewport().update(width, height, true);
     }
 
     @Override
     public void render() {
-        spriteBatch.begin();
         ScreenUtils.clear(0.1686f, 0.1686f, 0.1686f, 1f);
+        stage.act();
         stage.draw();
+        scrollPane.layout();
+
+        logLabel.setText(logs.toString());
+
+        spriteBatch.begin();
         personagem.update(Gdx.graphics.getDeltaTime());
         personagem.render(spriteBatch);
         spriteBatch.end();
-
-        // Atualiza o log
-        logLabel.setText(logs.toString()); // Atualiza o conteúdo do log
-        scrollPane.layout();// Atualiza o layout do ScrollPane
-    }
-
-    private void drawBackground() {
-        BackGround background = new BackGround(0, Gdx.graphics.getHeight() - 440);
-        stage.addActor(background);
-    }
-
-    private void drawButton() {
-        Button button = new Button(325, Gdx.graphics.getHeight() - 470);
-        stage.addActor(button);
     }
 
     @Override
@@ -104,12 +127,10 @@ public class Main extends ApplicationAdapter {
         return fonte;
     }
 
-    // Função para adicionar mensagens ao log
     public void adicionarLog(String mensagem) {
         logs.append(mensagem).append("\n");
     }
 
-    // Exemplo de como adicionar logs
     public void exemploDeLogs() {
         adicionarLog("Iniciando o jogocfksdnl...");
         adicionarLog("Iniciando o jogo...");
